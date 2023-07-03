63ad8515b4f622aa56a252192a8618d038fbe162
Rizky-Nov <rizky04noviwidiyanto04@gmail.com>
2023-07-03 09:35:52 +0700

    perubahan pada view dan comment model dan controller

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -4,29 +4,29 @@ from app import db
 
 def index() :
 	# user = db.session.execute(db.select(user_model).order_by(user_model.nama)).scalars()
-	user = user_model.query.all()
-	return render_template('app.html', users=user)
+	# user = user_model.query.all()
+	return render_template('app.html')
 
-def add(data) :
-	nama = data['nama']
-	email = data['email']
-	print(nama, email)
-	db.session.add(user_model(nama=nama, email=email))
-	db.session.commit()
+# def add(data) :
+# 	nama = data['nama']
+# 	email = data['email']
+# 	print(nama, email)
+# 	db.session.add(user_model(nama=nama, email=email))
+# 	db.session.commit()
 
-def edit(data) :
-	id = data['id']
-	nama = data['nama']
-	email = data['email']
-	print(nama, email, id)
-	# user = db.get_or_404(user_model, id)
-	user = user_model.query.filter_by(id=id).first()
-	user.nama = nama
-	user.email = email
-	db.session.commit()
-	return 'success'
+# def edit(data) :
+# 	id = data['id']
+# 	nama = data['nama']
+# 	email = data['email']
+# 	print(nama, email, id)
+# 	# user = db.get_or_404(user_model, id)
+# 	user = user_model.query.filter_by(id=id).first()
+# 	user.nama = nama
+# 	user.email = email
+# 	db.session.commit()
+# 	return 'success'
 
-def delete(id) :
-	user = user_model.query.filter_by(id=id).first()
-	db.session.delete(user)
-	db.session.commit()
+# def delete(id) :
+# 	user = user_model.query.filter_by(id=id).first()
+# 	db.session.delete(user)
+# 	db.session.commit()
--- app/routes/web.py
+++ app/routes/web.py
@@ -6,18 +6,18 @@ from ..controllers import auth_controller
 def index() :
 	return auth_controller.index()
 
-@app.route('/users', methods=['POST'])
-def user_add() :
-	if request.method == 'POST':
-		auth_controller.add(request.form)
-		return redirect('/')
+# @app.route('/users', methods=['POST'])
+# def user_add() :
+# 	if request.method == 'POST':
+# 		auth_controller.add(request.form)
+# 		return redirect('/')
 
-@app.route('/users/edit', methods=['POST'])
-def user_edit() :
-	user = auth_controller.edit(request.form)
-	return redirect('/')
+# @app.route('/users/edit', methods=['POST'])
+# def user_edit() :
+# 	user = auth_controller.edit(request.form)
+# 	return redirect('/')
 
-@app.route('/users/delete/<int:id>')
-def user_delete(id) :
-	auth_controller.delete(id)
-	return redirect('/')
+# @app.route('/users/delete/<int:id>')
+# def user_delete(id) :
+# 	auth_controller.delete(id)
+# 	return redirect('/')
